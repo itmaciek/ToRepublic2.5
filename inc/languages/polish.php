9e3986decd6914be9c37c0f1dd2ddc0755bc7718
gholol <admin@2.pl>
2016-01-04 18:10:16 +0100

    tr2.5

---
+++ inc/languages/polish.php
@@ -0,0 +1,24 @@
+<?php
+// The friendly name of the language
+$langinfo['name'] = "Polski";
+
+// The author of the language
+$langinfo['author'] = "Polski Support MyBB";
+
+// The language authors website
+$langinfo['website'] = "http://www.mybboard.pl/";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1806";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "pl";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
